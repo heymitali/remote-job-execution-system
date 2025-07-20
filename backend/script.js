24d02a2af5909ed11996bd78d00337d82cb3bd58
heymitali <mitalisinghnaruka@gmail.com>
2025-07-21 02:15:04 +0530

    remote command execution poc success

---
+++ backend/script.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+app.get('/ping', (req, res) => {
+    res.send('pong!');
+});
---
+++ backend/testScript.js
@@ -0,0 +1,27 @@
+
+const { readFileSync } = require('fs');
+const { homedir } = require('os');
+const { join } = require('path');
+
+const { Client } = require('ssh2');
+
+const conn = new Client();
+conn.on('ready', () => {
+  console.log('Client :: ready');
+  conn.exec('pwd', (err, stream) => {
+    if (err) throw err;
+    stream.on('close', (code, signal) => {
+      console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+      conn.end();
+    }).on('data', (data) => {
+      console.log('STDOUT: ' + data);
+    }).stderr.on('data', (data) => {
+      console.log('STDERR: ' + data);
+    });
+  });
+}).connect({
+  host: '4.213.155.245',
+  port: 22,
+  username: 'mitali',
+  privateKey: readFileSync(join(homedir(), '.ssh', '1753042297_244287'))
+});
